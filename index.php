56feba756481e5e6b4462ac8b419c14bcc2e7495
eimantaspav <63236210+eimantaspav@users.noreply.github.com>
2020-08-10 18:16:46 +0300

    Added 'asign/unasign project/employee'

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 $servername = "127.0.0.1";
 $username = "root";
 $password = "mysql";
-$db = "projectstaff";
+$db = "Projectstaff";
 
 $conn = mysqli_connect($servername, $username, $password, $db);
 
@@ -51,7 +51,7 @@ if (isset($_POST['updatePName'])) {
 }
 
 
-// Delete  staff by ID
+// Delete row by ID
 if (isset($_POST['del'])) {
   $delID = $_POST['del'];
   $sql = "DELETE FROM staff WHERE id=$delID";
@@ -66,6 +66,46 @@ if (isset($_POST['delP'])) {
   mysqli_query($conn, $sql);
   header("Refresh:0");
 }
+
+// Asign employee 
+if (isset($_POST['aEmployee'])) {
+  $projectName = $_POST['asignProject'];
+  $employeeName = $_POST['asignEmployee'];
+  $sql = "UPDATE projects SET Employee='$employeeName'
+  WHERE Project='$projectName'";
+  mysqli_query($conn, $sql);
+  header("Refresh:0");
+}
+
+// Unasign employee
+if (isset($_POST['unasignE'])) {
+  $delE = $_POST['unasignE'];
+  $sql = "UPDATE projects SET Employee=''
+  WHERE Employee='$delE'";
+  mysqli_query($conn, $sql);
+  header("Refresh:0");
+}
+
+// Asign project 
+if (isset($_POST['aProject'])) {
+  $projectName = $_POST['asignProject'];
+  $employeeName = $_POST['asignEmployee'];
+  $sql = "UPDATE staff SET Project='$projectName'
+  WHERE Name='$employeeName'";
+  mysqli_query($conn, $sql);
+  header("Refresh:0");
+}
+
+
+// Unasign project 
+if (isset($_POST['unasignP'])) {
+  $delP = $_POST['unasignP'];
+  $sql = "UPDATE staff SET Project=''
+  WHERE Project='$delP'";
+  mysqli_query($conn, $sql);
+  header("Refresh:0");
+}
+
 
 ?>
 <!DOCTYPE html>
@@ -92,20 +132,19 @@ if (isset($_POST['delP'])) {
     </div>
 
   </div>
-
-
   <?php
-  echo '<table class="table">';
 
-  $sqlProjects = "SELECT ID, Project FROM projects";
+  $sqlProjects = "SELECT ID, Project, Employee FROM projects";
   $result = $conn->query($sqlProjects);
+  // PROJEKTAI PAGE
   if ($_SERVER["REQUEST_URI"] == '/PersonalasMYSQL/' or $_GET["path"] != 'darbuotojai') {
     if ($result->num_rows > 0) {
+      echo '<table class="table">';
       echo
         "<tr>
     <td class='small-col'>ID</td>
     <td>Project</td>
-    <td>Asigned</td>
+    <td>Asigned Employee</td>
     <td class='small-col'>Actions</td>;
     </tr>";
       while ($row = $result->fetch_assoc()) {
@@ -115,26 +154,54 @@ if (isset($_POST['delP'])) {
             <input type='hidden' name='updatePName' value='" . $row["ID"] . "'>
             <input type='text' name='pname' value=''>
             <input  class='buttons' type='submit' value='CHANGE PROJECT NAME'>
-           </form>" . "</td> 
-          <td>Asigned##</td>
-          <td class='small-col'><form method='POST'>
+           </form>" . "</td> " . 
+           "<td>" . $row["Employee"] . "<form method='POST'>
+           <input type='hidden' name='unasignE' value='" . $row["Employee"] . "'>
+           <input type='submit' value='UNASIGN'>
+          </form></td>
+           
+        </td>
+            <td class='small-col'><form method='POST'>
           <input type='hidden' name='delP' value='" . $row["ID"] . "'>
           <input  class='buttons delete' type='submit' value='DELETE'>
          </form></td>
 
         </tr>";
       }
+      echo '</table><br>';
+
+
+      // ASIGN EMPLOYEE 
+      echo "<form method='POST'>";
+
+      $queryStaff = mysqli_query($conn, "SELECT * FROM staff");
+      echo '<select name="asignEmployee">';
+
+      while ($row = mysqli_fetch_array($queryStaff)) {
+        echo "<option value='" . $row['Name'] . "'>" . $row['Name'] . "</option>";
+      }
+      echo '</select>';
+
+      $queryProject = mysqli_query($conn, "SELECT * FROM projects");
+      echo '<select name="asignProject">';
+      while ($row = mysqli_fetch_array($queryProject)) {
+        echo "<option value='" . $row['Project'] . "'>" . $row['Project'] . "</option>";
+      }
+      echo "</select><input type='hidden' name='aEmployee'>
+      <input  class='buttons' type='submit' value='ASIGN EMPLOYEE'></form>";
     } else {
       echo "0 results";
     }
+    // DARBUOTOJAI PAGE 
   } else if ($_GET["path"] = 'darbuotojai') {
-    $sqlProjects = "SELECT ID, Name FROM staff";
+    $sqlProjects = "SELECT ID, Name, Project FROM staff";
     $result = $conn->query($sqlProjects);
+    echo '<table class="table">';
 
     echo "<tr>
     <td class='small-col'>ID</td>
     <td>Name</td>
-    <td>Asigned Course</td>
+    <td>Asigned Project</td>
     <td class='small-col'>Action</td>
 
     </tr>";
@@ -147,7 +214,10 @@ if (isset($_POST['delP'])) {
           <input type='text' name='name' value=''>
           <input  class='buttons' type='submit' value='CHANGE NAME'>
          </form>" . "</td>" .
-          '<td>Asigned Course</td>' .
+          "<td>" . $row["Project"] . "<form method='POST'>
+          <input type='hidden' name='unasignP' value='" . $row["Project"] . "'>
+          <input type='submit' value='UNASIGN'>
+         </form> </td>" .
           "<td class='small-col'><form method='POST'>
           <input type='hidden' name='del' value='" . $row["ID"] . "'>
           <input  class='buttons delete' type='submit' value='DELETE'>
@@ -155,9 +225,28 @@ if (isset($_POST['delP'])) {
          </td>
           </tr>";
     }
+    echo '</table><br>';
+
+    // ASIGN PROJECT 
+    echo "<form method='POST'>";
+
+    $queryStaff = mysqli_query($conn, "SELECT * FROM staff");
+    echo '<select name="asignEmployee">';
+
+    while ($row = mysqli_fetch_array($queryStaff)) {
+      echo "<option value='" . $row['Name'] . "'>" . $row['Name'] . "</option>";
+    }
+    echo '</select>';
+
+    $queryProject = mysqli_query($conn, "SELECT * FROM projects");
+    echo '<select name="asignProject">';
+    while ($row = mysqli_fetch_array($queryProject)) {
+      echo "<option value='" . $row['Project'] . "'>" . $row['Project'] . "</option>";
+    }
+    echo "</select><input type='hidden' name='aProject'>
+    <input  class='buttons' type='submit' value='ASIGN PROJECT'></form>";
   }
 
-  echo '</table><br>';
 
   if ($_SERVER["REQUEST_URI"] == '/PersonalasMYSQL/' or $_GET["path"] != 'darbuotojai') {
     echo '<form method="POST">
@@ -170,13 +259,6 @@ if (isset($_POST['delP'])) {
 <input type="submit" value="ADD NEW MEMBER">
 </form>';
   }
-
-  echo '<p class="currentDir">Current directory: ' . $_GET["path"] . '</p>';
-  echo '<p class="currentDir">Current directory: ' . $_SERVER["REQUEST_URI"] . '</p>';
-  echo '<p class="currentDir">Current directory: ' . $_SERVER["QUERY_STRING"] . '</p>';
-
-
-
   $conn->close();
 
   ?>
